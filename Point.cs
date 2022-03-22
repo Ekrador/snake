0f9d27df080067cad64d918950c60fc94e0d1483
Ekrador <wowezlo@gmail.com>
2022-03-22 17:14:52 +0500

    Add lines

---
+++ Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char symb;
+        public Point()
+        {
+
+        }
+        public Point(int _x, int _y, char _symb)
+        {
+            x = _x;
+            y = _y;
+            symb = _symb;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace snake
 {
@@ -6,7 +7,22 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This lesson one!");
+            Point p1 = new Point(1,5,'*');
+          
+
+            p1.Draw();
+
+            Point p2 = new Point(3, 6, '#');
+            p2.Draw();
+            Point p3 = new Point(5, 6, '$');
+            p3.Draw();
+
+
+            HorizontalLines first = new HorizontalLines(1,33,7,'_');
+            VerticalLines second = new VerticalLines(1, 1, 7, '|');
+            second.Draw();
+            first.Draw();
+
             Console.ReadLine();
         }
     }
