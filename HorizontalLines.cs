5aec1691cfc9b0a9c695ea84fa7a989dd1b8be66
Ekrador <wowezlo@gmail.com>
2022-03-22 17:15:12 +0500

    Lines

---
+++ HorizontalLines.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class HorizontalLines
+    {
+        List<Point> plist;
+        public HorizontalLines(int x_left, int x_right, int y, char sym)
+        {
+            plist = new List<Point>();
+   
+            for (int i =x_left; i <= x_right; i++)
+            {
+                Point p = new Point(i,y,sym);
+                plist.Add(p);
+
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point i in plist)
+            {
+                i.Draw();
+            }
+        }
+    }
+}
---
+++ VerticalLines.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class VerticalLines
+    {
+        List<Point> plist;
+
+        public VerticalLines(int x, int y_up, int y_down, char symb)
+        {
+            plist = new List<Point>();
+            for (int i = y_up; i <= y_down; i++)
+            {
+                Point p = new Point(x, i, symb);
+                plist.Add(p);
+            }
+
+        }
+
+        public void Draw()
+        {
+            foreach(Point p in plist)
+            {
+                p.Draw();
+            }
+        }
+
+    }
+}
