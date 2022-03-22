bb4bb797541b654e3e00330f75db057bd7dcc796
Ekrador <wowezlo@gmail.com>
2022-03-22 15:32:55 +0500

    lesson2

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This lesson one!");
+            int x1 = 1;
+            int y1 = 4;
+            char sym1 = '*';
+            Draw(x1, y1, sym1);
+
             Console.ReadLine();
+        }
+        static void Draw(int x, int y, char symb)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
         }
     }
 }
